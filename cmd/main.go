edaf60f39b5ac1b7efaab76277e81bc507170538
Nikita-Ignatenkov <117105477+Nikita-Ignatenkov@users.noreply.github.com>
2025-01-24 18:12:39 +0500

    поднял базу

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,49 @@ package main
 import (
 	"github.com/Nikita-Ignatenkov/todo-app"
 	"github.com/Nikita-Ignatenkov/todo-app/pkg/handler"
+	"github.com/Nikita-Ignatenkov/todo-app/pkg/repository"
+	"github.com/Nikita-Ignatenkov/todo-app/pkg/service"
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+	"github.com/subosito/gotenv"
 	"log"
+	"os"
 )
 
 func main() {
-	hendlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
+
+	if err := gotenv.Load(); err != nil {
+		log.Fatalf("error loading env vars: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", hendlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+
 }
